4d2e2aa9fbf0cd0dbface0c792792c56fbc6d8cc
Nelson Aloysio <26492660+nelsonaloysio@users.noreply.github.com>
2024-08-24 03:20:24 +0200

    PEP 621 support.

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+
+<!--
+## \[Version\] - YYYY-MM-DD
+### Added
+### Changed
+### Deprecated
+### Fixed
+### Removed
+-->
+
+## \[1.0\] - 2024-MM-DD
+- First release (WIP).
--- src/networkx_temporal/__init__.py
+++ src/networkx_temporal/__init__.py
@@ -1,19 +1,23 @@
 from .__version__ import __version__
 from .convert import convert
 from .draw import draw
-from .graph import (TemporalGraph,
-                    TemporalDiGraph,
-                    TemporalMultiGraph,
-                    TemporalMultiDiGraph)
-from .io import (read_graph,
-                 write_graph)
-from .transform import (from_events,
-                        from_snapshots,
-                        from_static,
-                        from_unified)
-
+from .graph import (
+    TemporalGraph,
+    TemporalDiGraph,
+    TemporalMultiGraph,
+    TemporalMultiDiGraph,
+)
+from .io import (
+    read_graph,
+    write_graph,
+)
+from .transform import (
+    from_events,
+    from_snapshots,
+    from_static,
+    from_unified,
+)
 __all__ = (
-    "__version__",
     "TemporalGraph",
     "TemporalDiGraph",
     "TemporalMultiGraph",
