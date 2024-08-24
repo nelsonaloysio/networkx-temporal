290d6d65dedede39cc48baad8c23151835b82b57
Nelson Aloysio <26492660+nelsonaloysio@users.noreply.github.com>
2024-08-24 03:20:24 +0200

    PEP 621 support.

--- src/networkx_temporal/__version__.py
+++ src/networkx_temporal/__version__.py
@@ -1,1 +1,8 @@
-__version__ = "1.0b16"
+try:
+    from importlib.metadata import version
+    __version__ = version("networkx_temporal")
+except ImportError: # <= Python 3.7
+    from os.path import abspath, dirname
+    PATH = abspath(f"{dirname(__file__)}/../..")
+    with open(f"{PATH}/pyproject.toml") as f:
+        __version__ = f.read().split("version = ", 1)[-1].split("\n", 1)[0].strip('"')
