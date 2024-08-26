12041881f3b1cb483cd616e9dd3759cd886fe431
Nelson Aloysio <26492660+nelsonaloysio@users.noreply.github.com>
2024-08-26 18:49:38 +0200

    Extended reader and writer functions to file objects and buffers.

--- src/networkx_temporal/io/writer.py
+++ src/networkx_temporal/io/writer.py
@@ -1,12 +1,15 @@
 import os
 import os.path as osp
 import zipfile
-from io import BytesIO
-from typing import Optional
+from io import BufferedWriter, BytesIO, StringIO, TextIOWrapper
+from typing import Callable, Optional, Union
 
 import networkx as nx
 
+from .utils import get_filepath, get_filename, get_format, get_format_ext, get_function
 from ..typing import TemporalGraph, Literal
+
+DEFAULT_FORMAT = "graphml"
 
 COMPRESSION = Literal[
     "ZIP_STORED",
@@ -18,35 +21,36 @@ COMPRESSION = Literal[
 
 def write_graph(
     TG: TemporalGraph,
-    path: str,
-    format: Optional[str] = None,
+    file: Optional[Union[str, BufferedWriter, BytesIO]] = None,
+    frmt: Optional[Union[str, Callable]] = None,
     makedirs: bool = False,
     compression: Optional[COMPRESSION] = None,
     compresslevel: Optional[int] = None,
     allowZip64: bool = False,
     **kwargs
-) -> None:
+) -> Union[bytes, None]:
     """
     Writes :class:`~networkx_temporal.TemporalGraph` to graph file or compressed
     `ZipFile <https://docs.python.org/3/library/zipfile.html#zipfile.ZipFile>`__
     containing multiple snapshots.
 
-    If the object contains more than one snapshot --- such as after calling
-    :func:`~networkx_temporal.TemporalGraph.slice` --- this function writes a single ZIP archive,
-    in which each file refers to a snapshot. Files within are named ``{name}_{t}.{format}``,
-    where ``t`` is the snapshot index and ``format`` is the file extension.
+    If the object contains more than one snapshot, such as after calling
+    :func:`~networkx_temporal.TemporalGraph.slice`, this function writes a single ZIP archive,
+    in which each file refers to a snapshot. Files within are named ``{name}_{t}.{ext}``,
+    where ``t`` is the snapshot index and ``ext`` is the extension format.
 
     .. seealso::
 
-        For a list of supported formats, see NetworkX's `read and write documentation
-        <https://networkx.org/documentation/stable/reference/readwrite/index.html>`__.
+        The `read and write documentation
+        <https://networkx.org/documentation/stable/reference/readwrite/index.html>`__
+        from NetworkX for a list of supported formats.
 
     :param TG: :class:`~networkx_temporal.TemporalGraph` to write.
-    :param path: Path to file. If it does not end with ''.zip'', it is automatically added as a suffix,
-        if required. Example: ``'path/to/file.graphml.zip'``.
-    :param str format: Extension format to use. Optional. If unset, it is inferred from ``path``.
-        Must be among the supported formats by NetworkX.
-    :param makedirs: Whether to create directories to path if they do not exist.
+    :param object file: Binary file-like object or string containing path to ZIP file. Optional. If
+        ``None`` (default), returns content as bytes.
+    :param frmt: Extension format or callable function to write graphs with. If unset and ``file``
+        is a string, it is inferred from it. Otherwise, defaults to ``'graphml'``.
+    :param makedirs: Whether to create directories to file if they do not exist.
         Default is ``False``.
     :param str compression: Compression method to use. Optional. The following values are accepted:
 
@@ -72,48 +76,54 @@ def write_graph(
         Default is ``False``.
     :param kwargs: Additional arguments to pass to NetworkX writer function.
     """
-    if path.endswith(".zip"):
-        name, ext = osp.splitext(path[:-4]) if format is None else (path[:-4], format)
-    else:
-        name, ext = osp.splitext(path) if format is None else (path, format)
+    path = get_filepath(file)
+    name = get_filename(path)
+    frmt = get_format(path, frmt)
+    func = get_function(frmt, "write")
 
-    ext = ext.lstrip(".")
-    func = getattr(nx, f"write_{ext}", None)
-    # func = getattr(nx, f"generate_{ext}", None)
+    if frmt is None:
+        frmt, func = DEFAULT_FORMAT, getattr(nx, f"write_{DEFAULT_FORMAT}")
 
-    assert ext,\
-        "Missing file extension to write graph, required if ``format`` is not set."
-
+    assert type(file) != str or not osp.isdir(file),\
+        "Argument `file` must be a file path or object, not a directory."
+    assert type(file) != TextIOWrapper,\
+        f"File must be opened in binary mode, received {type(file)} but expected {BufferedWriter}."
+    assert type(file) != StringIO,\
+        f"Buffer must be binary, received {type(file)} but expected {BytesIO}."
+    assert frmt is not None,\
+        "Missing extension format to write graph in file name or `frmt` parameter."
+    assert type(frmt) == str or callable(frmt),\
+        f"Argument `frmt` must be a string or callable function, received: {type(frmt)}."
     assert func is not None,\
-        f"Extension '{format}' is not supported by NetworkX. Supported formats: "\
+        f"Extension '{frmt}' is not supported by NetworkX. Supported formats: "\
         f"{[f.split('write_', 1)[-1] for f in dir(nx) if f.startswith('write_')]}."
-
     assert compression is None or compression.upper() in COMPRESSION.__args__,\
         f"Argument `compression` must be among {COMPRESSION.__args__}."
-
-    compression = getattr(zipfile, (compression or "ZIP_STORED").upper())
-
-    if makedirs:
-        os.makedirs(osp.dirname(path), exist_ok=True)
 
     if type(TG) in (nx.Graph, nx.DiGraph, nx.MultiGraph, nx.MultiDiGraph):
         TG = [TG]  # Allows a single graph to be passed as input.
 
-    if len(TG) == 0:
-        raise ValueError("TemporalGraph is empty.")
+    if makedirs and name is not None:
+        os.makedirs(osp.dirname(file), exist_ok=True)
 
-    if len(TG) == 1 and not path.endswith(".zip"):
-        func(TG[0], path, **kwargs)
-        return
+    compression = getattr(zipfile, (compression or "ZIP_STORED").upper())
+    file, nofile = (BytesIO(), True) if file is None else (file, False)
+    name = osp.basename(TG.name or name or 'snapshot')
+    ext = get_format_ext(frmt)
 
-    zipped = zipfile.ZipFile(path, "w",
-                             compression=compression,
-                             compresslevel=compresslevel,
-                             allowZip64=allowZip64)
+    with zipfile.ZipFile(file, "w",
+                         compression=compression,
+                         compresslevel=compresslevel,
+                         allowZip64=allowZip64) as zf:
 
-    with zipped as z:
         for t, G in enumerate(TG):
-            filename = f"{osp.basename(name)}{f'_{t}' if len(TG) > 1 else ''}.{ext}"
+            filename = f"{name}{f'_{t}' if len(TG) > 1 else ''}{ext}"
+
             with BytesIO() as buffer:
                 func(G, buffer, **kwargs)
-                z.writestr(filename, buffer.getvalue())
+                zf.writestr(filename, buffer.getvalue())
+
+    if type(file) == BytesIO:
+        file.seek(0)
+
+    return file.read() if nofile else None
