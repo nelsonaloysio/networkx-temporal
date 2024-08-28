3174dd6c003cb6fd38a0bcc5807b771a13d42683
Nelson Aloysio <26492660+nelsonaloysio@users.noreply.github.com>
2024-08-28 02:31:29 +0200

    Fixed `__version__` for `Python<3.8`.

--- src/networkx_temporal/__version__.py
+++ src/networkx_temporal/__version__.py
@@ -1,8 +1,7 @@
+__version__ = ""
 try:
     from importlib.metadata import version
     __version__ = version("networkx_temporal")
 except ImportError: # <= Python 3.7
-    from os.path import abspath, dirname
-    PATH = abspath(f"{dirname(__file__)}/../..")
-    with open(f"{PATH}/pyproject.toml") as f:
-        __version__ = f.read().split("version = ", 1)[-1].split("\n", 1)[0].strip('"')
+    from pkg_resources import get_distribution
+    __version__ = get_distribution("networkx_temporal").version
