5bfd261162d68ee3a8fec45623bbd62c072750c2
Nelson Aloysio <26492660+nelsonaloysio@users.noreply.github.com>
2024-11-21 16:44:56 +0100

    Improved `typing` module compatibility with `python==3.7,>=3.12`.

--- src/networkx_temporal/typing/typing.py
+++ src/networkx_temporal/typing/typing.py
@@ -1,16 +1,4 @@
-from typing import TypeAlias, Union
-
-try:
-    from typing import Literal
-except ImportError: # Python < 3.8
-    from typing import _GenericAlias, _SpecialForm
-    class _LiteralForm(_SpecialForm, _root=True):
-        def __repr__(self):
-            return "typing_extensions." + self._name
-        def __getitem__(self, parameters):
-            return _GenericAlias(self, parameters)
-    Literal = _LiteralForm("Literal", doc="typing_extensions.Literal")
-Literal.__doc__ = "Type hint for ``typing.Literal`` with Python 3.7 support."
+from typing import Union
 
 import networkx as nx
 
@@ -24,11 +12,24 @@ class _TemporalGraphType:
         return "networkx_temporal.TemporalGraph"
 _TemporalGraphType = _TemporalGraphType()
 
-Figure: TypeAlias = _FigureType
+try:
+    from typing import Literal
+    Figure: type = _FigureType
+    StaticGraph: type = Union[nx.Graph, nx.DiGraph, nx.MultiGraph, nx.MultiDiGraph]
+    TemporalGraph: type = _TemporalGraphType
+except ImportError: # Python < 3.8
+    from typing import _GenericAlias, _SpecialForm
+    class _LiteralForm(_SpecialForm, _root=True):
+        def __repr__(self):
+            return "typing_extensions." + self._name
+        def __getitem__(self, parameters):
+            return _GenericAlias(self, parameters)
+    Literal = _LiteralForm("Literal", doc="typing_extensions.Literal")
+    Figure = type(_FigureType)
+    StaticGraph = Union[nx.Graph, nx.DiGraph, nx.MultiGraph, nx.MultiDiGraph]
+    TemporalGraph = type(_TemporalGraphType)
+
+Literal.__doc__ = "Type hint for ``typing.Literal`` with Python 3.7 support."
 Figure.__doc__ = "Type hint for ``matplotlib.figure.Figure`` objects."
-
-StaticGraph: TypeAlias = Union[nx.Graph, nx.DiGraph, nx.MultiGraph, nx.MultiDiGraph]
 StaticGraph.__doc__ = "Union type hint for any ``networkx`` graph objects."
-
-TemporalGraph: TypeAlias = _TemporalGraphType
 TemporalGraph.__doc__ = "Type hint for any ``networkx_temporal`` graph objects."
