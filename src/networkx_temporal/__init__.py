7794dad8670fa2a1ee42dff2b80bfc9d7c4d37d7
Nelson Aloysio <26492660+nelsonaloysio@users.noreply.github.com>
2024-11-21 15:07:31 +0100

    Added new functions and top-level module docstring.

--- src/networkx_temporal/__init__.py
+++ src/networkx_temporal/__init__.py
@@ -1,24 +1,44 @@
+"""
+Top-level module for the package.
+
+.. rubric:: Summary
+
+.. autosummary::
+
+   drawing
+   graph
+   io
+   metrics
+   transform
+   typing
+   utils
+
+.. rubric:: Note
+
+Some classes and functions are exposed at the top level for convenience and may also be accessed via the
+individual modules. For example, the :class:`~networkx_temporal.graph.TemporalGraph` class
+can be imported by running:
+
+.. code-block:: python
+
+    >>> from networkx_temporal import TemporalGraph
+
+Or, alternatively, from the :mod:`~networkx_temporal.graph` module in which it is defined:
+
+.. code-block:: python
+
+    >>> from networkx_temporal.graph import TemporalGraph
+
+For detailed information on each class and function, please refer to the individual modules.
+"""
+
 from .__version__ import __version__
-from .convert import convert
-from .draw import draw
-from .graph import (
-    TemporalGraph,
-    TemporalDiGraph,
-    TemporalMultiGraph,
-    TemporalMultiDiGraph,
-    is_temporal_graph,
-    temporal_graph,
-)
-from .io import (
-    read_graph,
-    write_graph,
-)
-from .transform import (
-    from_events,
-    from_snapshots,
-    from_static,
-    from_unified,
-)
+from .drawing import *
+from .graph import *
+from .io import *
+from .transform import *
+from .utils import *
+
 __all__ = (
     "TemporalGraph",
     "TemporalDiGraph",
@@ -26,12 +46,15 @@ __all__ = (
     "TemporalMultiDiGraph",
     "convert",
     "draw",
+    "is_frozen",
     "is_temporal_graph",
     "from_events",
+    "from_multigraph",
     "from_snapshots",
     "from_static",
     "from_unified",
     "read_graph",
     "temporal_graph",
+    "to_multigraph",
     "write_graph",
 )
