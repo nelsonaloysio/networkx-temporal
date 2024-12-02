3a0c0016eaaa20345faf19cd9bbe71a09c5f7125
Nelson Aloysio <26492660+nelsonaloysio@users.noreply.github.com>
2024-12-02 15:00:25 +0100

    Updated type hinting from `typing` module.

--- src/networkx_temporal/typing/__init__.py
+++ src/networkx_temporal/typing/__init__.py
@@ -27,11 +27,10 @@ and may not be used to check an object type at runtime, as the following example
 
 .. note::
 
-   The convenience function :func:`~networkx_temporal.graph.is_temporal_graph` may be used to check
-   if an object is an instance of a :class:`~networkx_temporal.graph.TemporalGraph`,
-   :class:`~networkx_temporal.graph.TemporalDiGraph`,
-   :class:`~networkx_temporal.graph.TemporalMultiGraph`
-   or :class:`~networkx_temporal.graph.TemporalMultiDiGraph` class.
+   The convenience functions :func:`~networkx_temporal.utils.is_temporal_graph`
+   and :func:`~networkx_temporal.utils.is_static_graph`
+   may also be used to check if an object is an instance of a temporal or
+   static NetworkX graph, respectively.
 """
 
 from .typing import (
--- src/networkx_temporal/typing/typing.py
+++ src/networkx_temporal/typing/typing.py
@@ -1,17 +1,4 @@
-from typing import Union
 from sys import version_info
-
-import networkx as nx
-
-class _FigureType:
-    def __repr__(self):
-        return "matplotlib.figure.Figure"
-_FigureType = _FigureType()
-
-class _TemporalGraphType:
-    def __repr__(self):
-        return "networkx_temporal.TemporalGraph"
-_TemporalGraphType = _TemporalGraphType()
 
 try:
     from typing import Literal
@@ -24,16 +11,32 @@ except ImportError: # Python < 3.8
             return _GenericAlias(self, parameters)
     Literal = _LiteralForm("Literal", doc="typing_extensions.Literal")
 
-if version_info[1] >= 12:
-    Figure: type = _FigureType
-    StaticGraph: type = Union[nx.Graph, nx.DiGraph, nx.MultiGraph, nx.MultiDiGraph]
-    TemporalGraph: type = _TemporalGraphType
+class _FigureType:
+    def __repr__(self):
+        return "Figure"
+_FigureType = _FigureType()
+
+class _StaticGraphType:
+    def __repr__(self):
+        return "Graph"
+_StaticGraphType = _StaticGraphType()
+
+class _TemporalGraphType:
+    def __repr__(self):
+        return "TemporalGraph"
+_TemporalGraphType = _TemporalGraphType()
+
+if version_info[1] >= 11:
+    from typing import TypeAlias
+    Figure: TypeAlias = _FigureType
+    StaticGraph: TypeAlias = _StaticGraphType
+    TemporalGraph: TypeAlias = _TemporalGraphType
 else:
     Figure = type(_FigureType)
-    StaticGraph = Union[nx.Graph, nx.DiGraph, nx.MultiGraph, nx.MultiDiGraph]
+    StaticGraph = type(_StaticGraphType)
     TemporalGraph = type(_TemporalGraphType)
 
 Literal.__doc__ = "Type hint for ``typing.Literal`` with Python 3.7 support."
 Figure.__doc__ = "Type hint for ``matplotlib.figure.Figure`` objects."
-StaticGraph.__doc__ = "Union type hint for any ``networkx`` graph objects."
+StaticGraph.__doc__ = "Type hint for any ``networkx`` graph objects."
 TemporalGraph.__doc__ = "Type hint for any ``networkx_temporal`` graph objects."
