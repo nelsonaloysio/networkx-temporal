ea382432dca574e531600a7e36daa79aa37f599e
Nelson Aloysio <26492660+nelsonaloysio@users.noreply.github.com>
2025-09-07 03:09:34 +0200

    Added support for the HIF standard.

--- src/networkx_temporal/readwrite/__init__.py
+++ src/networkx_temporal/readwrite/__init__.py
@@ -4,15 +4,18 @@ Input/Output functions for temporal graphs.
 .. autosummary::
 
    read_graph
+   read_hif
    write_graph
 
 .. rubric:: Functions
 """
 
+from .hif import read_hif
 from .reader import read_graph
 from .writer import write_graph
 
 __all__ = (
     "read_graph",
+    "read_hif",
     "write_graph",
 )
